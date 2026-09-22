e7465df0c4c18a4bd65851114e73c74dca2d1e70
Axel <axel.olsson@insurello.se>
2026-09-22 08:11:54 +0200

    Remove outdated example (#31)

--- example/src/DesignSystem/Views.elm
+++
@@ -1,8 +0,0 @@
-module DesignSystem.Views exposing (horizontalFiller)
-
-import Element exposing (Element)
-
-
-horizontalFiller : Element msg
-horizontalFiller =
-    Element.el [ Element.width Element.fill ] Element.none
--- example/src/Shared/Types/WindowSize.elm
+++
@@ -1,7 +0,0 @@
-module Shared.Types.WindowSize exposing (..)
-
-import UiExplorer
-
-
-type alias WindowSize =
-    UiExplorer.PageSize
